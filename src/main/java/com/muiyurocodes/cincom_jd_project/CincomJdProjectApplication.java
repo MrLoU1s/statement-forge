b7a5d193ca3f79ae5f45c864dd724dc1132d0ab2
MrLoU1s <muiyurolouis@gmail.com>
2026-07-10 14:14:27 +0300

    chore: raw Spring Initializr scaffold (Boot 4.1.0, JPA, Flyway, Postgres, Testcontainers)

---
+++ src/main/java/com/muiyurocodes/cincom_jd_project/CincomJdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.muiyurocodes.cincom_jd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CincomJdProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CincomJdProjectApplication.class, args);
+    }
+
+}
